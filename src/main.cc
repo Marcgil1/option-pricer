4bc4ff915b86e8d89163f541242892dfd58ca7df
marcgil1 <marcos.gil@proton.me>
2023-11-18 15:24:55 +0100

    Refactor functionality into pricer class

--- src/main.cc
+++ src/main.cc
@@ -1,43 +1,109 @@
 #include <functional>
 #include <iostream>
 #include <numeric>
+#include <memory>
 #include <random>
 #include <vector>
 
-int numTrials = 10000000;
-double alpha = 0.06;
-double beta = 0.3;
-double initialValue = 5;
-double optionStrikePrice = 10;
-double startTime = 0;
-double endTime = 1;
-double totalTime = endTime - startTime;
+int    numTrials  = 10000000;
 double returnRate = 0.06;
 
-double exactOptionValue(double w) {
-	return initialValue * exp((alpha - 0.5*beta*beta)*totalTime + beta*w);
+class ExactConstantLinearEuropeanPutPricer {
+	public:
+		struct Params {
+			double alpha = 0.06;
+			double beta = 0.3;
+			double initialValue = 5;
+			double strikePrice = 10.0;
+			double totalTime = 1.0;
+		};
+		
+	private:
+		std::unique_ptr<std::default_random_engine>       generator;
+		std::unique_ptr<std::normal_distribution<double>> distribution;
+		Params params;
+
+	public:
+		ExactConstantLinearEuropeanPutPricer(
+			std::unique_ptr<std::default_random_engine>       generator,
+			std::unique_ptr<std::normal_distribution<double>> distribution)
+			: generator   (std::move(generator))
+			, distribution(std::move(distribution))
+			, trialVals   (std::make_unique<std::vector<double>>())
+			{}
+			
+		std::unique_ptr<std::vector<double>>
+		calculateTrialVals(unsigned numTrials);
+
+	private:
+		std::vector<double>                  rndNums;
+		std::unique_ptr<std::vector<double>> trialVals;
+
+		void   calcRandomNumbers   (unsigned numTrials);
+		void   calcOptionValuations(unsigned numTrials);
+		double calcOptionValuation (double   rnd);
+		double calcExactOptionValue(double   w);
+
+};
+
+void
+ExactConstantLinearEuropeanPutPricer::calcRandomNumbers(unsigned numTrials) {
+	rndNums.clear();
+	rndNums.resize(numTrials, 0.0);
+
+	for (int trial = 0; trial < numTrials; trial++)
+		rndNums[trial] = (*distribution)(*generator);
+}
+
+void
+ExactConstantLinearEuropeanPutPricer::calcOptionValuations(unsigned numTrials) {
+	trialVals->clear();
+	trialVals->resize(numTrials, 0.0);
+	for (int trial = 0; trial < numTrials; trial++)
+		trialVals->at(trial) = calcOptionValuation(rndNums[trial]);
+}
+
+double
+ExactConstantLinearEuropeanPutPricer::calcExactOptionValue(double w) {
+	return
+		params.initialValue
+		* std::exp(
+			(params.alpha - 0.5*params.beta*params.beta)*params.totalTime
+			+ params.beta*w
+ 		);
+}
+
+double
+ExactConstantLinearEuropeanPutPricer::calcOptionValuation(double rnd) {
+	auto w = rnd * std::sqrt(params.totalTime);
+	return std::max(
+		params.strikePrice - calcExactOptionValue(w),
+		0.0
+	);
+}
+
+std::unique_ptr<std::vector<double>>
+ExactConstantLinearEuropeanPutPricer::calculateTrialVals(unsigned numTrials) {
+	calcRandomNumbers   (numTrials);
+	calcOptionValuations(numTrials);
+
+	return std::move(trialVals);
 }
 
 int main() {
-	std::default_random_engine generator;
-	std::normal_distribution<double> distribution(0.0, 1.0);
-	auto rnd = std::bind(distribution, generator);
+	auto generator    = std::make_unique<std::default_random_engine>();
+	auto distribution = std::make_unique<std::normal_distribution<double>>(0.0, 1.0);
+	ExactConstantLinearEuropeanPutPricer optionPricer(std::move(generator), std::move(distribution));
 
-	std::vector<double> rndNums(numTrials, 0.0);
-	for (int trial = 0; trial < numTrials; trial++) {
-		rndNums[trial] = rnd();
-	}
-
-	std::vector<double> trialVals(numTrials, 0.0);
-	for (int trial = 0; trial < numTrials; trial++) {
-		auto w = rndNums[trial] * std::sqrt(totalTime);
-		auto stockPrice = exactOptionValue(w);
-		trialVals[trial] = std::max(optionStrikePrice - stockPrice, 0.0);
-	}
-
+	auto optionVals =
+		optionPricer.calculateTrialVals(numTrials);
 	auto meanOptionVal =
-		std::accumulate(std::begin(trialVals), std::end(trialVals), 0.0) / numTrials;
-	auto discountedOptionVal = exp(-returnRate * totalTime) * meanOptionVal;
+		std::accumulate(
+			optionVals->begin(),
+			optionVals->end(), 0.0)
+		/ numTrials;
+	auto discountedOptionVal =
+		std::exp(-returnRate * 1.0) * meanOptionVal;
 
 	std::cout << discountedOptionVal << std::endl;
 }
